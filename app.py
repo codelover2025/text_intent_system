43d404612b44cae37922a9511928e3c29d2ae8dd
CHIRAG SAXENA <109552580+codelover2025@users.noreply.github.com>
2025-05-06 21:49:40 +0530

    Initial commit for Flask app deployment

--- app.py
+++ app.py
@@ -8,4 +8,7 @@ def home():
     return send_from_directory('static', 'index.html')  # Serve frontend
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    
+ port = int(os.environ.get("PORT", 5000))
+app.run(host='0.0.0.0', port=port, debug=True)
+
